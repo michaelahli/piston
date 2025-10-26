2b89ca6fda2647c9531c1f8854fc0e262db5f615
Michael <michael.ahli99@gmail.com>
2025-10-26 16:00:37 +0700

    preinstall python modules
    install cannot be done on flight

--- packages/python/3.12.0/build.sh
+++ packages/python/3.12.0/build.sh
@@ -18,4 +18,15 @@ cd ..
 
 rm -rf build
 
-bin/pip3 install numpy scipy pandas pycryptodome whoosh bcrypt passlib sympy xxhash base58 cryptography PyNaCl
+bin/pip3 install \
+  openstoxlify whoosh sympy PyNaCl numpy pandas scipy matplotlib seaborn plotly \
+  statsmodels ta bt quantstats empyrical vectorbt pyfolio finta scikit-learn xgboost \
+  lightgbm catboost tensorflow torch pytorch-forecasting darts mlflow \
+  optuna arch pmdarima prophet hmmlearn pystan pymc3 pymc cvxpy pulp simpy deap \
+  pygad riskfolio-lib sqlalchemy sqlite3 psycopg2 pymongo cryptography pycryptodome \
+  bcrypt passlib xxhash base58 pyarrow fastparquet polars fastapi flask celery \
+  apscheduler docker pydantic loguru dash streamlit gradio tqdm joblib multiprocessing \
+  numba rich colorama yfinance ccxt alpaca-trade-api ib-insync requests aiohttp \
+  beautifulsoup4 lxml mlxtend quantlib finquant qstrader backtrader zipline pyfolio \
+  statsforecast pywavelets ruptures quantconnect quantstats-lite backtesting qtpylib \
+  talib pytrendlines quanttools ta-lib tensortrade
