6087efa73312bcc4b709d1b142741735c124413c
Michael <michael.ahli99@gmail.com>
2025-11-08 12:57:12 +0700

    extend package python 3.13.9
    it will be used as stoxlify's python compiler

---
+++ packages/python/3.13.9/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build
+
+cd build
+
+curl "https://www.python.org/ftp/python/3.13.9/Python-3.13.9.tgz" -o python.tar.gz
+tar xzf python.tar.gz --strip-components=1
+rm python.tar.gz
+
+./configure --prefix "$PREFIX" --with-ensurepip=install
+make -j$(nproc)
+make install -j$(nproc)
+
+cd ..
+
+rm -rf build
+
+bin/pip3 install \
+  numpy scipy pandas pycryptodome whoosh bcrypt passlib sympy xxhash base58 cryptography PyNaCl \
+  openstoxlify pandas matplotlib seaborn plotly statsmodels ta bt quantstats vectorbt finta scikit-learn \
+  xgboost lightgbm catboost tensorflow torch pytorch-forecasting darts mlflow optuna arch \
+  prophet hmmlearn cvxpy pulp simpy deap pygad riskfolio-lib sqlalchemy pymongo pyarrow \
+  fastparquet polars celery apscheduler docker loguru tqdm joblib numba yfinance ccxt alpaca-trade-api \
+  ib-insync requests aiohttp beautifulsoup4 lxml mlxtend quantlib finquant qstrader backtrader \
+  statsforecast pywavelets ruptures quantconnect backtesting quanttools ta-lib
